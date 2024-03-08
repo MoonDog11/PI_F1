49646be71ca3560cc5705cdcce5e916261b81432
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-08 16:12:38 -0500

    Update driverRoute.js

--- server/src/routes/driverRoute.js
+++ server/src/routes/driverRoute.js
@@ -9,7 +9,7 @@ const router = Router();
 
 // Ruta para obtener todos los conductores
 router.get("/drivers", getAllDriversController);
-router.get("/drivers/:idDriver", getDriverByIdController);
+router.get("/drivers/id/:idDriver", getDriverByIdController);
 router.get("/drivers/:name", getDriverByNameController);
 router.post("/drivers", createDriver);
 
