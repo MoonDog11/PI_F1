172dec248a6825d7c5d31fbd45b128e66b376783
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-09 12:05:17 -0500

    Update driverHandler.js

--- server/src/Handlers/driverHandler.js
+++ server/src/Handlers/driverHandler.js
@@ -1,67 +1,40 @@
 const { Driver, Team } = require("../db");
-const { Op } = require("sequelize");
-const axios = require("axios");
 
-const getAllDriversHandler = async (req, res) => {
+const getAllDriversHandler = async () => {
   try {
     const drivers = await Driver.findAll();
-    res.json(drivers);
+    return drivers;
   } catch (error) {
     console.error("Error al obtener los conductores desde el handler:", error);
-    res.status(500).send("Error al obtener los conductores desde el handler");
-  }
-};
-
-const createDriverHandler = async (req, res) => {
-  try {
-    await createDriverController(req, res);
-  } catch (error) {
-    console.error("Error en el controlador del conductor:", error);
-    res.status(500).json({ error: "Error interno del servidor" });
+    throw error;
   }
 };
 
 const getDriverByIdHandler = async (idDriver) => {
   try {
-    const driver = await Driver.findByPk(idDriver, {
-      include: Team,
-    });
-
+    const driver = await Driver.findByPk(idDriver, { include: Team });
     return driver;
   } catch (error) {
-    console.error(
-      "Error al obtener el conductor por ID desde el handler:",
-      error
-    );
+    console.error("Error al obtener el conductor por ID desde el handler:", error);
     throw error;
   }
 };
+
 const getDriverByNameHandler = async (name) => {
   try {
     console.log("Searching for driver in the database with name:", name);
-    // Convertir el nombre a minúsculas antes de la búsqueda
     const lowercaseName = name.toLowerCase();
+    const drivers = await Driver.findAll({
+      where: {
+        [Op.or]: [
+          { 'name.forename': { [Op.like]: `%${lowercaseName}%` } },
+          { 'name.surname': { [Op.like]: `%${lowercaseName}%` } }
+        ]
+      },
+      include: Team,
+    });
 
-    // Verificar si el parámetro parece ser solo un apellido
-    const isSurname = name.includes(".");
-
-    // Construir la parte de la URL según si es un apellido o un nombre
-    let url;
-    if (isSurname) {
-      url = `http://localhost:5000/drivers?name.surname=${lowercaseName}`;
-    } else {
-      url = `http://localhost:5000/drivers?name.forename=${lowercaseName}`;
-    }
-
-    console.log("API URL:", url);
-
-    // Realizar la solicitud a la API
-    const response = await fetch(url);
-    const data = await response.json();
-
-    console.log("API Response inside action:", data);
-
-    return data;
+    return drivers;
   } catch (error) {
     console.error("Error al buscar conductores por nombre:", error);
     throw error;
@@ -70,24 +43,17 @@ const getDriverByNameHandler = async (name) => {
 
 const searchDriversByTeamHandler = async (team) => {
   try {
-    // Aquí puedes realizar cualquier manipulación o procesamiento adicional de los conductores si es necesario
     console.log("Equipo proporcionado:", team);
-
-    // Simplemente devolvemos un objeto de ejemplo por ahora
-    return { message: `Drivers encontrados para el equipo ${team}` };
+    // Implementa la lógica para buscar conductores por equipo aquí
   } catch (error) {
-    console.error(
-      "Error en la búsqueda de conductores por equipo:",
-      error.message
-    );
+    console.error("Error en la búsqueda de conductores por equipo:", error.message);
     throw error;
   }
 };
 
 module.exports = {
-  createDriverHandler,
   getAllDriversHandler,
-  getDriverByNameHandler,
   getDriverByIdHandler,
+  getDriverByNameHandler,
   searchDriversByTeamHandler,
 };
