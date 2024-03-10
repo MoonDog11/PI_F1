d8867eb9cabdace0469a57bc21359942af2739bc
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-10 11:38:26 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require("./src/db.js");
-const { saveDriversToDB } = require("./src/Controllers/driverController"); // Import both functions
 
-const PORT = 3002;
+
+
 
 conn
   .sync({ alter: true })
   .then(() => {
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
-      saveDriversToDB();
     });
   })
   .catch((error) => console.error(error));
