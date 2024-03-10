1d0f43eaf63e46c608f58f6131a4077e910e3f1e
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-10 11:36:49 -0500

    Update driverRoute.js

--- server/src/routes/driverRoute.js
+++ server/src/routes/driverRoute.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+const fs = require("fs");
+const path = require("path");
 const {
   getAllDriversController,
   getDriverByNameController,
@@ -7,6 +9,21 @@ const {
 } = require("../Controllers/driverController");
 
 const router = Router();
+
+router.get("/database", (req, res) => {
+  try {
+    // Leer el archivo db.json
+    const dbPath = path.join(__dirname, "../api/db.json");
+    const data = fs.readFileSync(dbPath, "utf8");
+    
+    // Convertir los datos a JSON y enviarlos como respuesta
+    const jsonData = JSON.parse(data);
+    res.json(jsonData);
+  } catch (error) {
+    console.error("Error al leer la base de datos:", error);
+    res.status(500).send("Error al leer la base de datos");
+  }
+});
 
 // Ruta para obtener todos los conductores
 router.get("/drivers", getAllDriversController);
