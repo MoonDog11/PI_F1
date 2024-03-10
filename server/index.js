46c58b744a9123147025bcd5a2fe6053cec709b8
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-10 12:45:52 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require("./src/db.js");
+require("dotenv").config();
 
 
 conn
   .sync({ alter: true })
   .then(() => {
-    server.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
+    server.listen(process.env.PORT, () => {
+      console.log(`Server listening on port, process.env.PORT);
     });
   })
   .catch((error) => console.error(error));
