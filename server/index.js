f2c91cf7168b96db416b36efa120ba009cc97185
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-12 12:06:53 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,26 @@
 const axios = require("axios");
 const server = require("./src/server");
-const { conn } = require("./src/db.js");
+const { conn, Driver } = require("./src/db.js"); // Importa Driver desde db.js
 require("dotenv").config();
-const { saveDataToDatabase } = require("./src/Controllers/driverController"); // Import both functions
-
-
+const { saveDataToDatabase } = require("./src/Controllers/driverController"); // Importa saveDataToDatabase desde driverController
 
 conn
   .sync({ alter: true })
   .then(() => {
     server.listen(process.env.PORT, async () => {
-  console.log(`Server listening on port, ${process.env.PORT}`);
-  const response = await axios.get("http://pif1-production.up.railway.app/drivers");
-  const drivers = response.data;
-  const saveResult = await saveDataToDatabase(Driver, drivers);
-  if (saveResult.success) {
-    console.log("Drivers saved successfully to database");
-  } else {
-    console.error("Error saving drivers to database");
-  }
-});
+      console.log(`Server listening on port, ${process.env.PORT}`);
+      try {
+        const response = await axios.get("http://pif1-production.up.railway.app/drivers");
+        const drivers = response.data;
+        const saveResult = await saveDataToDatabase(Driver, drivers); // Usa Driver aquí
+        if (saveResult.success) {
+          console.log("Drivers saved successfully to database");
+        } else {
+          console.error("Error saving drivers to database");
+        }
+      } catch (error) {
+        console.error("Error fetching or saving drivers:", error);
+      }
+    });
+  })
+  .catch((error) => console.error(error));
