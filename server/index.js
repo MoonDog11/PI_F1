708ee75adb2ad69e4697f44912309fba0694f1e3
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-12 13:26:13 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,26 +1,17 @@
 const axios = require("axios");
 const server = require("./src/server");
-const { conn, Driver } = require("./src/db.js"); // Importa Driver desde db.js
-require("dotenv").config();
-const { saveDataToDatabase } = require("./src/Controllers/driverController"); // Importa saveDataToDatabase desde driverController
+const { conn } = require("./src/db.js");
+const { saveDriversToDB } = require("./src/Controllers/driverController"); // Import both functions
+
+const PORT = 3002;
 
 conn
   .sync({ alter: true })
   .then(() => {
-    server.listen(process.env.PORT, async () => {
-      console.log(`Server listening on port, ${process.env.PORT}`);
-      try {
-        const response = await axios.get("http://pif1-production.up.railway.app/drivers");
-        const drivers = response.data;
-        const saveResult = await saveDataToDatabase(Driver, drivers); // Usa Driver aquí
-        if (saveResult.success) {
-          console.log("Drivers saved successfully to database");
-        } else {
-          console.error("Error saving drivers to database");
-        }
-      } catch (error) {
-        console.error("Error fetching or saving drivers:", error);
-      }
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+      saveDriversToDB();
     });
   })
   .catch((error) => console.error(error));
+
