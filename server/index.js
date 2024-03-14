8b9f1f698efc118cd3dcf506b06b3beb4f21aa5a
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-14 10:31:45 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,24 +2,14 @@ const { exec } = require('child_process');
 const server = require("./src/server");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
-const { PORT, DB_HOST, DB_PORT, DB_USER, DB_NAME } = process.env;
+const { saveDriversToDB } = require("./src/Controllers/driverController"); 
 
 conn
-  .sync()
+  .sync({ alter: true })
   .then(() => {
-    // Generar respaldo de la base de datos
-    const dumpCommand = `pg_dump -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} -d ${DB_NAME} -Fc > database_dump.dump`;
-    exec(dumpCommand, (error, stdout, stderr) => {
-      if (error) {
-        console.error('Error generating database backup:', error);
-        return;
-      }
-      console.log('Database backup generated successfully.');
-    });
-
-    // Iniciar el servidor
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
+      saveDriversToDB();
     });
   })
   .catch((error) => console.error(error));
