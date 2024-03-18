77146ec517fc01f2a6b98aefb3cbbb9c6fec0f97
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-18 12:40:23 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = require('./src/server');
 const { conn } = require('./src/db.js');
-const { createDriverController, saveDriversToLocalhost, getAllDriversController, getDriverByNameController, getDriverByIdController, searchDriversByTeamController, getAllTeamsController, fetchDataFromLocalhost, saveDriversToLocalhost } = require("./src/Controllers/driverController.js");
+const { createDriverController, saveDriversToLocalhost, getAllDriversController, getDriverByNameController, getDriverByIdController, searchDriversByTeamController, getAllTeamsController, fetchDataFromLocalhost } = require("./src/Controllers/driverController.js");
 
 require('dotenv').config();
 
