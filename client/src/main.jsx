48ae76b1181da320f55ddefd1d9d913ac9c018f9
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-19 16:47:48 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from "././Redux/Store";// Asegúrate de importar tu store correctamente
+import store from "./Redux/Store";
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
+  document.getElementById('root')
 );
