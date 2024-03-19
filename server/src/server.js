5aaf5cb5057e77e822861ef4219b90a8e82f242b
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-19 17:13:44 -0500

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,8 @@
+
+To add CORS headers to your Express server, you can create a middleware function that adds the necessary headers to the response. Here's how you can modify your code to include this middleware:
+
+javascript
+Copy code
 const express = require("express");
 const router = require("./routes");
 const morgan = require("morgan");
@@ -7,9 +12,14 @@ const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
-server.use(cors({
-  origin: "https://pi-f1.vercel.app"
-}));
+
+// Custom CORS middleware
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://pi-f1.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 server.use(router);
 
