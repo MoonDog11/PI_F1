583c509f75eb51d750cd846e4bba55c782d46d79
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-19 11:09:23 -0500

    Update driverRoute.js

--- server/src/routes/driverRoute.js
+++ server/src/routes/driverRoute.js
@@ -4,26 +4,15 @@ const {
   getDriverByNameController,
   getDriverByIdController,
   createDriverController,
-  saveDriversToDB,
- saveDriversToLocalhost,
   getAllDriversFromRailwayController,
 } = require("../Controllers/driverController");
 
 const router = Router();
 
-// Ruta para obtener todos los conductores
 router.get("/drivers", getAllDriversController);
-
-// Ruta para obtener un conductor por su ID
 router.get("/drivers/id/:idDriver", getDriverByIdController);
-
-// Ruta para obtener un conductor por su nombre
 router.get("/drivers/name/:name", getDriverByNameController);
-
-// Ruta para crear un nuevo conductor
 router.post("/drivers", createDriverController);
-
-
 router.get("/drivers/local", getAllDriversFromRailwayController);
 
 
