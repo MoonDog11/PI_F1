85852fc15a8c479c0b0c8445be91c7b0cddb67cb
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-19 19:45:46 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://pif1-production.up.railway.app', // Cambia esto por la URL de tu backend en Railway
+        target: 'https://pif1-production.up.railway.app/drivers', // Cambia esto por la URL de tu backend en Railway
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '') // Elimina el prefijo /api de las solicitudes
