b48f68ada36428f8676bf1010b96165fef6ae906
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-19 19:37:34 -0500

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
-
 const router = Router();
+
+// Aquí importa y utiliza tus rutas específicas
+const driverRoutes = require("./driverRoute.js");
+router.use(driverRoutes);
 
 module.exports = router;
