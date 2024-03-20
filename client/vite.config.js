84a2eb61faa3a454d2f0abc6aae32662cea95b69
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-19 19:22:35 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://my-app.up.railway.app', // Cambia esto por la URL de tu backend en Railway
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '') // Elimina el prefijo /api de las solicitudes
+      }
+    }
+  }
+});
