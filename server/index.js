b9ec11532552830a901ece65e9fb36d1dd8697a1
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-19 19:53:53 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { conn } = require('./src/db.js');
 const driverRoutes = require('./src/routes/driverRoute.js');
+const cors = require('cors'); // Importa el middleware CORS
 
 require('dotenv').config();
 
 const { PORT } = process.env;
 const app = express();
 
+// Aplica el middleware CORS solo a las rutas relacionadas con los conductores
+app.use('/drivers', cors()); // Esto asegura que el middleware CORS solo se aplique a las rutas relacionadas con los conductores
 app.use(driverRoutes);
 
 app.listen(PORT, () => {
