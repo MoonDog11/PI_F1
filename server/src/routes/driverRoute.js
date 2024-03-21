2545166e71d9be4ac0bd79fef423b87758aca82c
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-20 19:22:31 -0500

    Update driverRoute.js

--- server/src/routes/driverRoute.js
+++ server/src/routes/driverRoute.js
@@ -10,9 +10,9 @@ const {
 const router = Router();
 
 router.get("/drivers", getAllDriversController);
+router.get("/drivers/:idDriver", getDriverByIdController);
 router.get("/drivers/local", getAllDriversFromRailwayController);
 router.post("/drivers", createDriverController);
-router.get("/drivers/:idDriver", getDriverByIdController);
 router.get("/drivers/:name", getDriverByNameController);
 
 
