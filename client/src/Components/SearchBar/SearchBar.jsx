789413016b30892da56680543e2dfae82c1e0e13
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-21 12:51:02 -0500

    Update SearchBar.jsx

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -7,7 +7,7 @@ import Card from '../Card/Card';
 const SearchBar = () => {
   const dispatch = useDispatch();
   const [searchQuery, setSearchQuery] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResult, setSearchResult] = useState(null);
 
   const handleChange = (event) => {
     setSearchQuery(event.target.value);
@@ -21,11 +21,9 @@ const SearchBar = () => {
     console.log('Search query:', searchQuery);
 
     try {
-      // Realiza la búsqueda y obtén los resultados
       const results = await dispatch(searchDriverByName(searchQuery));
       console.log('Search results:', results);
-      // Actualiza el estado searchResults con los resultados de la búsqueda
-      setSearchResults(results);
+      setSearchResult(results.length > 0 ? results[0] : null);
     } catch (error) {
       console.error('Error en la búsqueda:', error);
     }
@@ -39,33 +37,23 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="driver-form-container-2">
+    <div>
       <form onSubmit={handleSearch}>
-        <h1 className="driver-finder-title">Driver Finder</h1>
-        <div className="search-bar-container">
-          <input
-            className="INPUT-2"
-            type="text"
-            placeholder="Search by name"
-            value={searchQuery}
-            onChange={handleChange}
-            onKeyPress={handleKeypress}
-          />
-          <button type="submit" className="submit">
-            <span className="button-text">Search</span>
-          </button>
-        </div>
+        <input
+          type="text"
+          placeholder="Search by name"
+          value={searchQuery}
+          onChange={handleChange}
+          onKeyPress={handleKeypress}
+        />
+        <button type="submit">Search</button>
       </form>
 
-     {/* Muestra las tarjetas de los conductores en la interfaz de usuario */}
-      {searchResults.length > 0 && (
+      {/* Muestra la tarjeta del conductor en la interfaz de usuario */}
+      {searchResult && (
         <div>
-          <h2>Search Results</h2>
-          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-            {searchResults.map(driver => (
-              <Card key={driver.id} driver={driver} />
-            ))}
-          </div>
+          <h2>Search Result</h2>
+          <Card driver={searchResult} />
         </div>
       )}
     </div>
