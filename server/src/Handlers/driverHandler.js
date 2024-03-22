f7202adbba40b8777158c87524cbe9d2454dc8dc
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-22 13:34:58 -0500

    Update driverHandler.js

--- server/src/Handlers/driverHandler.js
+++ server/src/Handlers/driverHandler.js
@@ -37,23 +37,20 @@ const getDriverByIdHandler = async (idDriver) => {
   }
 };
 
-const getDriverByNameHandler = async (name) => {
+const getDriverByNameController = async (req, res) => {
   try {
-    console.log("Searching for driver in the database with name:", name);
-
-    const url = `https://pif1-production.up.railway.app/drivers/name/${name}`;
-
-    console.log("API URL:", url);
-
-    const response = await axios.get(url);
-    const data = response.data;
-
-    console.log("API Response inside action:", data);
-
-    return data;
+    const { name } = req.params;
+    const drivers = await getDriverByNameHandler(name);
+    
+    // Verificar si la respuesta es un objeto JSON válido
+    if (typeof drivers === 'object' && drivers !== null) {
+      res.json(drivers);
+    } else {
+      throw new Error('La respuesta no es un objeto JSON válido');
+    }
   } catch (error) {
-    console.error("Error al buscar conductores por nombre:", error);
-    throw error;
+    console.error('Error en la búsqueda de conductores:', error);
+    res.status(500).json({ error: 'Error en la búsqueda de conductores' });
   }
 };
 
