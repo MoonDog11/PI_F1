71e4f093f1fedee1ececf1a18c8e1ac2151c34f5
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-23 18:45:54 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ require('dotenv').config();
 const { PORT } = process.env;
 const app = express();
 
-// Aplica el middleware CORS solo a las rutas relacionadas con los conductores
-// app.use('/drivers', cors()); // Elimina esto
+app.use(express.json()); // Debes mover esto arriba de app.use(driverRoutes) para que el body-parser se aplique antes de las rutas de controladores
 
-app.use(express.json()); // Mueve esto arriba de app.use(driverRoutes)
+// Aplica el middleware CORS solo a las rutas relacionadas con los conductores
+app.use('/drivers', cors()); // Aquí deberías aplicar el middleware CORS a las rutas relacionadas con los controladores
 
 app.use(driverRoutes);
 
