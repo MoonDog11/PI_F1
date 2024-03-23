ef760f5b764db7587782fd3c3474d6b208a2bf20
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-03-23 18:35:04 -0500

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -10,13 +10,10 @@ server.use(express.json());
 
 // Configuración global de CORS
 server.use(cors({
-  origin: 'https://pi-f1.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: 'https://pi-f1.vercel.app', // Origen permitido
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'] // Encabezados permitidos
 }));
-
-// Middleware de manejo de preflight options global
-server.options('*', cors());
 
 // Utiliza tus rutas
 server.use(router);
